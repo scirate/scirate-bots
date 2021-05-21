1c6dd772f5ac74dda2618abc01a2a1c3f977aec7
Mehul Arora <aroram18@mcmaster.ca>
2021-05-21 23:25:02 +0530

    added some new features

--- discord/index.py
+++ discord/index.py
@@ -20,31 +20,40 @@ async def on_ready():
 
 @bot.command()
 @has_permissions(manage_roles=True, ban_members=True)
-async def top(ctx):
-    global called, break_loop    
+async def top(ctx, *, arg=None):
+    global called, break_loop 
     res=[]
-    if called:
-        msg = await ctx.reply('`Session Already Active`')
-        return await msg.delete(delay=5)
-    else: 
-        await ctx.reply('`Starting Subscription`')   
-        called, break_loop = True, False
-        while True and not break_loop:
-            embed = None
-            embed = await _fetch(ctx, res)
-            await ctx.send(embed = embed)
-            await asyncio.sleep(24*60*60)  
+    while arg:
+        if len(arg) > 20:
+            return await ctx.reply('`Query Character Limit Exceeded`')
+        elif called:
+            msg = await ctx.reply('`Session Already Active`')
+            return await msg.delete(delay=5)    
+        break          
+    await ctx.reply('`Starting Subscription`')   
+    called, break_loop = True, False
+    while True and not break_loop:
+        embed = None
+        embed = await _fetch(ctx, res, arg)     
+        await ctx.send(embed = embed)
+        await asyncio.sleep(2)  
 
 @bot.command()
 @has_permissions(manage_roles=True, ban_members=True)
 async def stop(ctx):
-    global called, break_loop    
+    global called, break_loop
+    if not called:
+        return await ctx.reply('`No Subscription To Cancel`')    
     break_loop, called = True, False
     return await ctx.reply('`Subscription Cancelled`')
      
-async def _fetch(ctx, res):
+async def _fetch(ctx, res, arg=None):
+    if arg:
+        render_link=f'https://scirate.com/search?utf8=%E2%9C%93&q={arg}'
+    elif arg == None:
+        render_link=f'https://scirate.com/'
     async with aiohttp.ClientSession() as session:
-        async with session.get('https://scirate.com/') as response:
+        async with session.get(render_link) as response:
             if response.status != 200:
                 await ctx.reply('`Falied to fetch data | Try again later`', mention_author=False)
             resp_text = await response.text()
@@ -52,11 +61,12 @@ async def _fetch(ctx, res):
             res = []            
             for title in soup.find_all("div", class_="title")[:10]:
                 res.append(f'[`{title.find("a").contents[0]}`]({"https://scirate.com" + title.find("a")["href"]})')               
+            res[0] = '•' + res[0]
             chars = [len(i) for i in res]
             if chars[0] > 2048:
                 res = res[:-1]
             embed = discord.Embed(title = f'`Top SciRate papers`', 
-                                        description = '\n'.join(res),
+                                        description = '\n•'.join(res),
                                         color = 0xe8e3e3)
             return embed
 
@@ -64,6 +74,8 @@ async def _fetch(ctx, res):
 @stop.error
 async def top_stop_error(ctx, error):
     if isinstance(error, MissingPermissions):
-        return await ctx.reply('Missing Permissions')            
+        return await ctx.reply('Missing Permissions') 
+    else:
+        pass           
 
 bot.run(config.TOKEN, bot=True, reconnect=True)
